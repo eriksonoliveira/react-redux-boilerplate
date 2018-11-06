8614d2421d7785ce1eb6bbe5d83d979e3f585169
Erikson Oliveira <eriksonmagno6@hotmail.com>
2018-11-06 14:52:30 -0500

    update gitignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 if (module.hot) {
   module.hot.accept();
