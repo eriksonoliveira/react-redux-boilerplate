d8629db29b02877dea8dd343668979ddb0d9327b
Erikson Oliveira <eriksonmagno6@hotmail.com>
2018-11-06 16:26:28 -0500

    create example

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import styles from "../styles/main.scss";
 import Button from "./Button/Button";
-import FancyButton from "./Fancy-button/Fancy-button";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Button label="Regular Button" />
-        <FancyButton label="Fancy Button" />
       </div>
     );
   }
